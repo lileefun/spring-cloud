59104fc4ea90605b74311beeb5f3694c311be9e2
李斌 <libin@owhat.cn>
2017-08-13 23:40:13 +0800

    commits

--- spring-cloud-provider/src/main/java/com/libin/controller/UserController.java
+++ spring-cloud-provider/src/main/java/com/libin/controller/UserController.java
@@ -4,6 +4,8 @@ import com.libin.model.UserDTO;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,13 +25,20 @@ public class UserController {
     }
 
     @Autowired
-    private EurekaClient discoveryClient;
+    private EurekaClient eurekaClient;
 
     @GetMapping("/eureka-instance")
     public String serviceUrl() {
-        InstanceInfo instance = discoveryClient.getNextServerFromEureka("APPNAME-LIBIN", false);
+        InstanceInfo instance = eurekaClient.getNextServerFromEureka("APPNAME-LIBIN", false);
         return instance.getHomePageUrl();
     }
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
+    @GetMapping("/eureka-info")
+    public ServiceInstance info() {
+        ServiceInstance localServiceInstance = discoveryClient.getLocalServiceInstance();
+        return localServiceInstance;
+    }
 }
