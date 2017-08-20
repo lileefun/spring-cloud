9bd56e2d972f61e9737db62b2b94a80eaaaa633e
李斌 <libin@owhat.cn>
2017-08-20 21:02:53 +0800

    commits

--- spring-boot-web/src/main/java/com/libin/test/BootStrap.java
+++ spring-boot-web/src/main/java/com/libin/test/BootStrap.java
@@ -1,14 +1,7 @@
 package com.libin.test;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.HttpMessageConverter;
 
 /**
  * Created by libin on 2017/8/18.
@@ -17,24 +10,11 @@ import org.springframework.http.converter.HttpMessageConverter;
  */
 
 @SpringBootApplication
-@MapperScan("com.libin.test.service.dao.mapper")
 public class BootStrap {
 
     public static void main(String[] args) {
         SpringApplication.run(BootStrap.class, args);
     }
 
-    /**
-     * 修改spring默认 jackjson  转换器为fastjsons
-     * @return
-     */
-    @Bean
-    public HttpMessageConverters fastJsonConverters() {
-        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
-        HttpMessageConverter httpMessageConverters = fastJsonHttpMessageConverter;
-        return new HttpMessageConverters(httpMessageConverters);
-    }
+
 }
--- spring-boot-web/src/main/java/com/libin/test/beanconf/BeanConfigures.java
+++ spring-boot-web/src/main/java/com/libin/test/beanconf/BeanConfigures.java
@@ -1,18 +1,39 @@
 package com.libin.test.beanconf;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.libin.test.api.ApiMethodService;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 
 /**
  * Created by libin on 2017/8/18.
  */
 @Configuration
+@MapperScan("com.libin.test.service.dao.mapper")
 public class BeanConfigures {
 
     @Bean
-    public ApiMethodService getApiMethodService (){
+    public ApiMethodService getApiMethodService() {
         return new ApiMethodService();
     }
 
+    /**
+     * 修改spring默认 jackjson  转换器为fastjsons
+     * @return
+     */
+    @Bean
+    public HttpMessageConverters fastJsonConverters() {
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter httpMessageConverters = fastJsonHttpMessageConverter;
+        return new HttpMessageConverters(httpMessageConverters);
+
+    }
 }
