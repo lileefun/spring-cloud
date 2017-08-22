092e751c1aea70f60c649e36cd050e342e5ef8f0
libin <libin@owhat.cn>
2017-08-22 13:38:37 +0800

    commits

--- spring-boot-web/src/main/java/com/libin/test/beanconf/RedisConfiguration.java
+++ spring-boot-web/src/main/java/com/libin/test/beanconf/RedisConfiguration.java
@@ -3,7 +3,6 @@ package com.libin.test.beanconf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
